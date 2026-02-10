920c8d05e683ac8b6517972a141a65fc9e529296
shhKnight30 <aishwarybatt@gmail.com>
2026-02-10 11:20:50 +0530

    Add WebSocket Service for LiveStream

---
+++ src/services/websocket.services.js
@@ -0,0 +1,31 @@
+import { Server, Socket } from "socket.io";
+
+class WebSocketService{
+    constructor(){
+        this.io = null
+        this.activeStreams = new Map()
+    }
+    initialize(server){
+        this.io = new Server(server,{
+            cors:{origin:"*"}
+        })
+        
+        this.io.on('connection',(socket)=>{
+            console.log('User connected: ',socket.id)
+            socket.on('join-stream',(streamId)=>{
+                socket.join(streamId)
+                console.log(`User ${socket.id} has joined the strea ${streamId}`)
+            })
+            
+            socket.on('leave-stream',(streamId)=>{
+                socket.leave(streamId)
+                console.log(`User ${socket.id} left stream ${streamId}`)
+            })
+        })
+    }
+    broadcastToStream(streamId,event,data){
+        this.io.to(streamId).emit(event,data)
+    }
+}
+
+export default new WebSocketService()
